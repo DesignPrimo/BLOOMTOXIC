dffca75c5a41f8bb91a2fe76c9834bebca0610f8
Primo <miprimomio@gmail.com>
2023-10-10 08:36:02 -0500

    Updates

---
+++ assets/404-68a07a65.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./AppLayoutStatus-f68628f9.js";import{o as r,l as t}from"./index-647917ed.js";import"./Spin-51fca547.js";const p={__name:"404",setup(c){return(e,_)=>(r(),t(o,{codigo:"Error 404",description:"Not found"}))}};export{p as default};
---
+++ assets/index-d74b0c29.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,e as c}from"./index-647917ed.js";const t={};function o(r,a){return n(),c("div",null," Index de data ")}const _=e(t,[["render",o]]);export{_ as default};
